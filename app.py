6ed2fbb21d8eec26644faca046a59d4f220fa115
Anjali Agarwal <agarwal.16@iitj.ac.in>
2023-05-17 16:47:55 +0530

    app.py

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 import pandas as pd
+
 app = Flask(__name__)
+
 @app.route('/')
 def home():
     return "Welcome to the Defect Tracking Tool"
 
 @app.route('/defects')
-
 def view_defects():
     # Read defect data from CSV using pandas
-    defect_data = pd.read_csv(r'C:\Users\Anjali\Desktop\Qc tool\creditcard.csv') 
+    defect_data = pd.read_csv(r'C:\Users\Anjali\Desktop\Qc tool\creditcard.csv')
 
     # Convert defect data to a list of dictionaries
     defects = defect_data.to_dict('records')
 
     return render_template('defects.html', defects=defects)
 
-
-
-
+if __name__ == '__main__':
+    app.run(debug=True)
